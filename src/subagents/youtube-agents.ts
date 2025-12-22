c1b4acf2faf4e37eb4f7b0a8d4745ff375108813
Connie Leung <cleung334@gmail.com>
2025-12-22 13:50:47 +0800

    fix: should be YouTube

--- src/subagents/youtube-agents.ts
+++ src/subagents/youtube-agents.ts
@@ -5,14 +5,14 @@ import { YoutubeHashtagsAgent } from './youtube-hashtags.agent';
 import { YoutubeTimelineAgent } from './youtube-timeline.agent';
 import { YoutubeTranscriptAgent } from './youtube-transcript.agent';
 
-const ParallelYoutubeAgent = new ParallelAgent({
+const ParallelYouTubeAgent = new ParallelAgent({
     name: "parallel_youtube_agent",
     subAgents: [YoutubeDescriptionAgent, YoutubeHashtagsAgent, YoutubeTimelineAgent],
     description: "Runs multiple Youtube agents in parallel to gather description, hashtags, and timeline."
 });
 
-export const SequentialYoutubeAgent = new SequentialAgent({
+export const SequentialYouTubeAgent = new SequentialAgent({
     name: 'sequential_youtube_agent',
-    subAgents: [YoutubeTranscriptAgent, ParallelYoutubeAgent, SendEmailAgent],
+    subAgents: [YoutubeTranscriptAgent, ParallelYouTubeAgent, SendEmailAgent],
     description: 'Runs Youtube agents sequentially to generate description based on transcript.',
 });
